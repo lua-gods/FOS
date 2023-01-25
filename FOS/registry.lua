9ae3ff2ab095c93c4882a16f07eb7432894a970b
dragekk <53555592+dragekk@users.noreply.github.com>
2023-01-25 12:27:07 +0100

    more spaces

--- FOS/registry.lua
+++ FOS/registry.lua
@@ -1,5 +1,5 @@
 local r = {
-   system_name="FOS",
+   system_name = "FOS",
    root_path = "", -- auto generated | sets the root path of all FOS services
    resolution = vectors.vec2(96,144), --12 * 8, 18 * 8
    font_texture_prefix = "font.", -- every texture with this prefix will be converted into a fontmap
