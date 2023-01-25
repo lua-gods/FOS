8d963329d8b10ce7ecf549cd2cbf7a96268a2792
dragekk <53555592+dragekk@users.noreply.github.com>
2023-01-25 14:32:40 +0100

    im tired of this bug, fix is good enough

--- phone.lua
+++ phone.lua
@@ -1,47 +1,47 @@
-models.FOS.phone.screen:setPrimaryRenderType("EMISSIVE_SOLID")
+models.FOS.phone.base.screen:setPrimaryRenderType("EMISSIVE_SOLID")
 local config = {
    third_person = {
-      pivot = vectors.vec3(4,22,0),
-      offset = vectors.vec3(1,-10,-10),
-      rotation = vectors.vec3(-100,0,0),
-      scale = vectors.vec3(0.4,0.4,0.4),
+      pivot = vectors.vec3(4, 22, 0),
+      offset = vectors.vec3(3, 10, -6),
+      rotation = vectors.vec3(-80, 0, 0),
+      scale = vectors.vec3(0.4, 0.4, 0.4),
    },
    first_person = {
-      pivot = vectors.vec3(8,0,0),
-      offset = vectors.vec3(8,2,-10),
-      rotation = vectors.vec3(-120,-60,0),
-      scale = vectors.vec3(0.8,0.8,0.8),
+      pivot = vectors.vec3(8, 0, 0),
+      offset = vectors.vec3(8, -1, -8),
+      rotation = vectors.vec3(-120, -60, 0),
+      scale = vectors.vec3(0.8, 0.8, 0.8),
    }
 }
 
 local function applyTransformPreset(preset)
-   models.FOS:setScale(preset.scale)
+   models.FOS.phone:setScale(preset.scale)
    if player:isLeftHanded() then
       models.FOS:setParentType("LEFT_ARM")
       models.FOS:setPivot(
-         -preset.pivot.x-preset.offset.x,
-         preset.pivot.y-preset.offset.y,
-         -preset.pivot.z-preset.offset.z)
-      models.FOS:setPos(-preset.offset.x,preset.offset.y,preset.offset.z)
-      models.FOS.phone:setRot(preset.rotation.x,-preset.rotation.y,-preset.rotation.z)
+         -preset.pivot.x,
+         preset.pivot.y,
+         -preset.pivot.z)
+      models.FOS.phone:setPos(-preset.offset.x,preset.offset.y,preset.offset.z)
+      models.FOS.phone.base:setRot(preset.rotation.x,-preset.rotation.y,-preset.rotation.z)
    else
       models.FOS:setParentType("RIGHT_ARM")
       models.FOS:setPivot(
-         preset.pivot.x-preset.offset.x,
-         preset.pivot.y-preset.offset.y,
-         preset.pivot.z-preset.offset.z)
-      models.FOS:setPos(preset.offset.x,preset.offset.y,preset.offset.z)
-      models.FOS.phone:setRot(preset.rotation)
+         preset.pivot.x,
+         preset.pivot.y,
+         preset.pivot.z)
+      models.FOS.phone:setPos(preset.offset.x,preset.offset.y,preset.offset.z)
+      models.FOS.phone.base:setRot(preset.rotation)
    end
 end
 
-events.ENTITY_INIT:register(function ()
+events.ENTITY_INIT:register(function()
    applyTransformPreset(config.third_person)
 end)
+
 if not host:isHost() then return end
 
-
-events.RENDER:register(function (td,context)
+events.RENDER:register(function(dt, context)
    if player:isLoaded() then
       if context == "FIRST_PERSON" then
          applyTransformPreset(config.first_person)
