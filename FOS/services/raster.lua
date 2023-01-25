43e13d7699535579dfe6e6d6a5f509c0383dc968
GNamimates <greenneon188@gmail.com>
2023-01-25 20:29:59 +0800

    funy UV color

--- FOS/services/raster.lua
+++ FOS/services/raster.lua
@@ -1,6 +1,8 @@
 -- purpose: handles the texture drawing
 
 local screen = textures:newTexture(FOS_REGISTRY.system_name..".screen",FOS_REGISTRY.resolution.x,FOS_REGISTRY.resolution.y)
-screen:fill(0,0,FOS_REGISTRY.resolution.x,FOS_REGISTRY.resolution.y,vec(1,0,0))
+screen:applyFunc(0,0,FOS_REGISTRY.resolution.x,FOS_REGISTRY.resolution.y,function (col, x, y)
+   return vec(x/FOS_REGISTRY.resolution.x,y/FOS_REGISTRY.resolution.y,0,1)
+end)
 screen:update()
 FOS_REGISTRY.screen_model:setPrimaryTexture("CUSTOM",screen)
