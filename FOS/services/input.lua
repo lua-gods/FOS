3ccbc467aced38d9b9dc3e4ded20150d2800da8b
dragekk <53555592+dragekk@users.noreply.github.com>
2023-02-08 19:57:14 +0100

    back button

--- FOS/services/input.lua
+++ FOS/services/input.lua
@@ -8,8 +8,12 @@ local key_names = {
     ["key.keyboard.up"] = "UP",
     ["key.keyboard.right"] = "RIGHT",
     ["key.keyboard.down"] = "DOWN",
-    ["key.keyboard.enter"] = "ENTER"
+    ["key.keyboard.enter"] = "ENTER",
+["key.keyboard.backspace"] = "BACK",
 }
+
+local can_press_keys = 0 -- 0 is true, 1 and above if false
+local go_to_home_time = 0
 
 local keys = {}
 local press
@@ -26,12 +30,12 @@ end
 
 
 function press(key)
-    if player:getItem(1).id ~= "minecraft:air" then
+    if can_press_keys >= 1 then
         return
     end
 
     local play_sound = false
-    local stop_sound = eventsManager.runEvent("KEY_PRESS", key)
+    eventsManager.runEvent("KEY_PRESS", key)
 
     local page = APP.app.pages[APP.app.current_page]
     local currently_selected = APP.app.selected_item
@@ -57,13 +61,20 @@ function press(key)
             page[currently_selected].pressAction()
             play_sound = true
         end
+    elseif key == "BACK" then
+        if go_to_home_time >= 1 and SYSTEM_REGISTRY.home_app ~= APP.app.id then
+            go_to_home_time = 0
+            APP.open(SYSTEM_REGISTRY.home_app)
+            play_sound = true
+        end
+        go_to_home_time = 10
     end
 
     if new_selected then
         APP.app.selected_item = new_selected
         raster.draw({currently_selected, new_selected})
     end
-    if play_sound and not stop_sound then
+    if play_sound then
         sounds:playSound("ui.button.click", player:getPos(), 0.25, 2)
     end
 end
@@ -71,6 +82,18 @@ end
 function input.isPressed(key)
     if keys[key] then
         return keys[key]:isPressed()
+    end
+end
+
+function events.tick()
+    go_to_home_time = math.max(go_to_home_time - 1, 0)
+
+    can_press_keys = math.max(can_press_keys - 1, 0)
+
+    if player:getItem(1).id ~= "minecraft:air" then
+        can_press_keys = 2
+    elseif host:isChatOpen() then
+        can_press_keys = 2
     end
 end
 
