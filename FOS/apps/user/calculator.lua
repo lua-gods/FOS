d912815796cda9f66dc51d29a9665d0b1f3e51e5
dragekk <53555592+dragekk@users.noreply.github.com>
2023-02-14 15:05:32 +0100

    working calculator??? what are you going to do? divide me by 0?

--- FOS/apps/user/calculator.lua
+++ FOS/apps/user/calculator.lua
@@ -1,34 +1,111 @@
 local app = APP.begin("calculator", "calculator")
 
--- SYSTEM_REGISTRY.home_app = "user:calculator"
-
+-- main page
 app.pages.main = {
     {type = "rectangle", size = vec(96, 8)},
-    {type = "rectangle", size = vec(96, 8), pos = vec(0, 16)},
-    {type = "text", text = "2+2*2", pos = vec(64, 16), size = 1},
     {type = "text", text = app.display_name},
+    {type = "rectangle", size = vec(96, 8), pos = vec(0, 16)},
+    {type = "text", text = "2+2*2", pos = vec(64, 16)}
 }
 
-local i = 0
-for y = 0, 3 do
+-- variables
+local input_text = ""
+local tile_pos = vec(0, 4)
+
+-- update text input
+local function input_text_update(str, redraw)
+    local offset = 94
+    for i = 1, #str do
+        local char = str:sub(i, i)
+        if char == "*" or char == "(" or char == ")" then
+            offset = offset - 5
+        elseif char == " " then
+            offset = offset - 3
+        elseif char == "." then
+            offset = offset - 2
+        else
+            offset = offset - 6
+        end
+    end
+
+    input_text = str
+
+    app.pages.main[4].text = str
+    app.pages.main[4].pos.x = offset
+
+    if redraw then
+        app.redraw()
+    end
+end
+
+-- tiles data
+local tiles_data = {
+    {"0"},
+    {"."},
+    {"<", function() input_text_update(input_text:sub(1, -2), true) end},
+    {"=", function()
+        local func = loadstring("return "..input_text)
+        if func then
+            local success, output = pcall(func)
+            if success then
+                input_text_update(tostring(output), true)
+                return
+            end
+        end
+        input_text_update("error", true)
+    end},
+    {"1"},
+    {"2"},
+    {"3"},
+    {"+"},
+    {"4"},
+    {"5"},
+    {"6"},
+    {"-"},
+    {"7"},
+    {"8"},
+    {"9"},
+    {"*"},
+    {"AC", function() input_text_update("", true) end},
+    {"()", function()
+        if input_text:sub(-1, -1):match("[0-9]") then
+            input_text_update(input_text..")", true)
+        else
+            input_text_update(input_text.."(", true)
+        end
+    end},
+    {" ^"},
+    {" /"},
+}
+
+-- generate tiles
+local tiles_start = #app.pages.main + 1
+local function pos_to_index(x, y)
+    return x + y * 4 + tiles_start, x + y * 4 + 1
+end
+
+for y = 0, 4 do
     for x = 0, 3 do
-        i = i + 1
-        app.pages.main[i + 4] = {
+        local i, i2 = pos_to_index(x, y)
+        app.pages.main[i] = {
             type = "rectangle",
-            color = i == 1 and "rectangle_select" or "rectangle",
-            size = vec(23 + (x == 3 and 1 or 0), 23),
-            pos = vec(x * 24, 121 - y * 24),
+            color = vec(x, y) == tile_pos and "rectangle_select" or "rectangle",
+            size = vec(x == 3 and 24 or 23, y == 4 and 12 or 23),
+            pos = vec(x * 24, (y == 4 and 132 or 121) - y * 24),
+        }
+
+        app.pages.main[i + 20] = {
+            type = "text",
+            text = tiles_data[i2][1],
+            size = y == 4 and 1 or 2,
+            pos = vec(x * 24 + 5, (y == 4 and 134 or 125) - y * 24),
         }
     end
 end
 
-local function pos_to_index(pos)
-    return pos.x + pos.y * 4 + 5
-end
-
-local pos = vec(0, 0)
+-- move and buttons
 function app.events.key_press(key)
-    local new_pos = pos:copy()
+    local new_pos = tile_pos:copy()
 
     if key == "LEFT" then
         new_pos.x = math.max(new_pos.x - 1, 0)
@@ -37,22 +114,32 @@ function app.events.key_press(key)
     elseif key == "DOWN" then
         new_pos.y = math.max(new_pos.y - 1, 0)
     elseif key == "UP" then
-        new_pos.y = math.min(new_pos.y + 1, 3)
+        new_pos.y = math.min(new_pos.y + 1, 4)
+    elseif key == "ENTER" then
+        local _, i = pos_to_index(tile_pos.x, tile_pos.y)
+        if tiles_data[i][2] then
+            tiles_data[i][2]()
+        else
+            input_text_update(input_text..tiles_data[i][1]:match("[^ ]"), true)
+        end
+        return
+    else
+        return
     end
 
-    app.pages.main[pos_to_index(pos)].color = "rectangle"
-    app.pages.main[pos_to_index(new_pos)].color = "rectangle_select"
+    local tile_pos_i = pos_to_index(tile_pos.x, tile_pos.y)
+    local new_pos_i = pos_to_index(new_pos.x, new_pos.y)
 
-    app.redraw({pos_to_index(pos), pos_to_index(new_pos)})
+    app.pages.main[tile_pos_i].color = "rectangle"
+    app.pages.main[new_pos_i].color = "rectangle_select"
 
-    pos = new_pos
+    app.redraw({tile_pos_i, new_pos_i})
+
+    tile_pos = new_pos
 end
 
---[[
-local fontManager = require(FOS_RELATIVE_PATH..".services.fontManager")
-print(fontManager.fonts.minimojangles["0"].width, "0")
-print(fontManager.fonts.minimojangles["*"].width, "*")
-print(fontManager.fonts.minimojangles["("].width, "(")
-print(fontManager.fonts.minimojangles[")"].width, ")")
-print(fontManager.fonts.minimojangles[" "].width, " ")
---]]
+-- init
+function app.events.init()
+    input_text_update("")
+    app.setPage("main")
+end
