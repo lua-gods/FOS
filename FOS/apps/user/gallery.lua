607b0b4d247e9a47b38cbb228761cf617adeaa87
dragekk <53555592+dragekk@users.noreply.github.com>
2023-02-17 18:56:28 +0100

    galleryyyyy

--- FOS/apps/user/gallery.lua
+++ FOS/apps/user/gallery.lua
@@ -25,7 +25,31 @@ local selected_photo_lines = {
     vec(1, 45, 44, 1),
 }
 
+app.pages.image_view = {
+    {type = "texture"},
+    {type = "rectangle", pos = vec(0, 140), size = vec(96, 4)},
+}
+
+app.pages.image_view_info = {
+    {type = "rectangle", size = vec(96, 16)},
+    {type = "text", text = "unknown", wrap_after = 96},
+    {type = "rectangle", color = "background", pos = vec(0, 16), size = vec(96, 144)},
+    {type = "text", text = "resolution: ", pos = vec(0, 24)},
+    {type = "text", text = "unknown", pos = vec(0, 32)},
+    {type = "rectangle", pos = vec(0, 128), size = vec(96, 16)},
+    {type = "text", text = "enter to\nremove photo", pos = vec(0, 128)},
+}
+
+local delete_photo
+app.pages.delete_photo_confirm = {
+    {type = "text", text = "are you sure you\nwant to remove\nthis photo?"},
+    {type = "rectangle", pos = vec(0, 128), size = vec(96, 16)},
+    {type = "text", pressAction = function() delete_photo() end, text = "yes", pos = vec(0, 128)},
+    {type = "text", pressAction = function() app.setPage("image_view") end, text = "no", pos = vec(0, 136)},
+}
+
 local pos = vec(0, 0)
+local selected_photo = -1
 
 local photos = {}
 
@@ -48,8 +72,32 @@ local function render_gallery()
     app.current_page = "empty"
     app.redraw(nil, true)
 
+    local y_limit = math.ceil(#photos * 0.5) - 1
+    local image_offset = pos.y * 2
+    local selected = pos.x + 1
+    local y_offset
+    if pos.y == 0 then
+        y_offset = 0
+    elseif pos.y == y_limit then
+        if #photos <= 4 then
+            y_offset = 0
+            image_offset = image_offset - 2
+            selected = selected + 2
+        else
+            y_offset = -8
+            image_offset = image_offset - 4
+            selected = selected + 4
+        end
+    elseif pos.y >= 1 then
+        image_offset = image_offset - 2
+        selected = selected + 2
+        y_offset = -4
+    end
+
+    selected_photo = image_offset + selected
+
     for i = 1, 6 do
-        local image = photos[i + pos.y * 2]
+        local image = photos[i + image_offset]
         if image then
             -- convert string to texture if needed
             if type(image[2]) == "string" then
@@ -63,7 +111,7 @@ local function render_gallery()
 
             -- set marker pos
             local marker = app.pages.image_tile[-i]
-            marker.pos.y = math.floor((i - 1) * 0.5) * 48 + 9
+            marker.pos.y = math.floor((i - 1) * 0.5) * 48 + 9 + y_offset
 
             -- set texture
             local center = marker.pos + marker.size * 0.5
@@ -76,9 +124,8 @@ local function render_gallery()
             app.current_page = "image_tile"
             app.redraw({-i}, true)
 
-            -- select
-            local isSelected = i == 1
-            if isSelected then
+            -- select outline
+            if selected == i then
                 app.current_page = "selected_photo"
                 for _, v in ipairs(selected_photo_lines) do
                     app.pages.selected_photo[1].pos = v.xy + marker.pos
@@ -95,25 +142,117 @@ local function render_gallery()
     app.redraw({1})
 end
 
+local function image_view_page()
+    local image = photos[selected_photo]
+    if not image then
+        return
+    end
+
+    if type(image[2]) == "string" then
+        local success, output = pcall(texture.read, image[2])
+        if success then
+            image[2] = output
+        else
+            return
+        end
+    end
+
+    local image_element = app.pages.image_view[1]
+    local dimensions = image[2]:getDimensions()
+
+    image_element.texture = image[2]
+    image_element.size = 96 / dimensions.x
+    image_element.pos = vec(0, (144 - image_element.size * dimensions.y) * 0.5)
+
+    app.pages.image_view_info[2].text = image[1]
+    app.pages.image_view_info[5].text = dimensions.x.."x"..dimensions.y
+
+    app.setPage("image_view")
+end
+
 local function setPage(page)
     if page == "main" or page == nil then
         app.selected_item = -1
-        pos = vec(0, 0)
-        get_photos()
         render_gallery()
+    elseif page == "image_view" then
+        image_view_page()
     else
         app.setPage(page)
     end
 end
 
 function app.events.open()
+    pos = vec(0, 0)
+    get_photos()
     setPage()
 end
 
 function app.events.close()
     photos = {}
+    app.pages.image_view[1].texture = nil
 end
 
-function app.events.tick()
+function app.events.key_press(key)
+    if app.current_page == "main" then
+        local old_pos = pos:copy()
+        local y_limit = math.ceil(#photos * 0.5) - 1
+        if key == "LEFT" then
+            pos.x = 0
+        elseif key == "RIGHT" then
+            pos.x = 1
+        elseif key == "UP" then
+            pos.y = math.max(pos.y - 1, 0)
+        elseif key == "DOWN" then
+            pos.y = math.min(pos.y + 1, y_limit)
+        elseif key == "ENTER" then
+            setPage("image_view")
+            return
+        end
+        
+        if pos.y == y_limit and pos.x == 1 and #photos % 2 == 1 then
+            pos.x = 0
+        end
+        
+        if old_pos ~= pos then
+            render_gallery()
+        end
+    elseif app.current_page == "image_view" then
+        local old_selected_photo = selected_photo
+        if key == "LEFT" then
+            selected_photo = math.max(selected_photo - 1, 0)
+        elseif key == "RIGHT" then
+            selected_photo = math.min(selected_photo + 1, #photos)
+        elseif key == "DOWN" then
+            pos = vec((selected_photo - 1) % 2, math.floor((selected_photo - 1) / 2))
+            setPage()
+            return
+        elseif key == "UP" then
+            setPage("image_view_info")
+        end
 
+        if old_selected_photo ~= selected_photo then
+            image_view_page()
+        end
+    elseif app.current_page == "image_view_info" then
+        if key == "DOWN" then
+            app.setPage("image_view")
+        elseif key == "ENTER" then
+            app.setPage("delete_photo_confirm")
+        end
+    end
+end
+
+function delete_photo()
+    local image = photos[selected_photo]
+    if image then
+        
+        if type(fileSystem.photos) == "table" then
+            fileSystem.photos[image[1]] = nil
+            fileSystem.save()
+        end
+    end
+
+    pos = vec(0, 0)
+    get_photos()
+    setPage()
 end
